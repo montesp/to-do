df8a5b6a79fa8ef1fd32a7b87c89276767787df8
Pavel <pavel.montes@pidelectronics.com>
2023-09-18 15:32:39 -0600

    Feature: onChange implemented

--- src/app/components/CreateTodoButton/CreateTodoButton.jsx
+++ src/app/components/CreateTodoButton/CreateTodoButton.jsx
@@ -1,13 +1,23 @@
-import React from "react"
-import { FaPlus } from "react-icons/fa6"
+"use client";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa6";
 
-function CreateTodoButton(){
+function CreateTodoButton() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    console.log("click");
+    setClick(!click);
+  };
+
   return (
     <button
-      className="fixed flex justify-center items-center bottom-4 right-4 rounded-full text-3xl w-10 h-10 bg-blue-400 text-zinc-950">
-        <FaPlus className="w-6 h-6"/>
+      onClick={handleClick}
+      className="fixed flex justify-center items-center bottom-4 right-4 rounded-full text-3xl w-10 h-10 bg-blue-400 text-zinc-950"
+    >
+      <FaPlus className="w-6 h-6" />
     </button>
-  )
+  );
 }
 
-export { CreateTodoButton }
+export { CreateTodoButton };
--- src/app/components/TodoSearch/TodoSearch.jsx
+++ src/app/components/TodoSearch/TodoSearch.jsx
@@ -1,12 +1,21 @@
-import React from "react"
+"use client";
+import React, { useState } from "react";
 
-function TodoSearch(){
+function TodoSearch() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const changeSearch = (e) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  };
+
   return (
     <input
+      onChange={(e) => changeSearch(e)}
       className="border-2 rounded-lg text-black  outline-0 pl-2 py-2 mb-3 min-w-[50%]"
       placeholder="Crear todo..."
     />
-  )
+  );
 }
 
-export { TodoSearch }
+export { TodoSearch };
