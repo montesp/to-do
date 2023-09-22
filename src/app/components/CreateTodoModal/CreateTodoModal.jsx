ae952273184c54259028a8c8d8b11638d95f5a80
Pavel Montes <pavelmontes22@gmail.com>
2023-09-22 10:06:30 -0600

    Refactor: Change html at jsx structure in ModalComponent

--- src/app/components/CreateTodoModal/CreateTodoModal.jsx
+++ src/app/components/CreateTodoModal/CreateTodoModal.jsx
@@ -18,7 +18,8 @@ function CreateTodoModal() {
     setNewDescription(description);
   };
 
-  const createNewTodo = () => {
+  const createNewTodo = (e) => {
+    e.preventDefault();
     if (title !== "" && description !== "") {
       createTodo(title, description);
       toggle();
@@ -29,14 +30,17 @@ function CreateTodoModal() {
 
   return (
     <div className="fixed top-0 right-0 w-full h-full flex justify-center items-center z-10 bg-black bg-opacity-60">
-      <div className="relative flex flex-col bg-white  w-11/12 md:w-3/5 lg:w-2/5 xl:w-2/5 px-8 py-4 rounded-xl  ">
+      <form
+        onSubmit={(e) => createNewTodo(e)}
+        className="relative flex flex-col bg-white  w-11/12 md:w-3/5 lg:w-2/5 xl:w-2/5 px-8 py-4 rounded-xl  "
+      >
         <button>
           x
           <FaX className="absolute right-4 top-4 w-4 h-4 font-bold  hover:text-red-500" />
         </button>
-        <h2 className="text-black text-center font-medium text-2xl mb-2">
+        <label className="text-black text-center font-medium text-2xl mb-2">
           Create a todo
-        </h2>
+        </label>
         <input
           onChange={(e) => getTitle(e)}
           className="text-black border-black border-2 p-2 rounded-lg mb-2"
@@ -52,19 +56,20 @@ function CreateTodoModal() {
         )}
         <div className="flex justify-around my-4 w-4/5 mx-auto ">
           <button
-            onClick={() => createNewTodo()}
-            className="bg-black text-white px-4 py-2 rounded-lg"
-          >
-            Create
-          </button>
-          <button
+            type="button"
             onClick={() => toggle()}
             className="bg-red-500 text-white px-4 py-2 rounded-lg"
           >
             Cancel
           </button>
+          <button
+            type="submit"
+            className=" text-black border-2 border-black px-4 py-2 rounded-lg hover:bg-black hover:text-white"
+          >
+            Create
+          </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
